4a5e35703b10bfe3b9a361940af6311572daa2c3
Pedro Sanches <pedro.sanches@oramix.pt>
2021-02-25 11:23:06 +0000

    new validations

--- pymodori.py
+++ pymodori.py
@@ -1,4 +1,4 @@
-#NEXT:
+## NEXT:
 # criar ficheiro config automatico para o caso de nao existir
 # excepçoes sons e imagens
 # impedir duas instancias
@@ -8,6 +8,7 @@
 # # detecta movimentos do rato
 # # detecta se bloqueia o Pc
 # # caso muito ativo durante muito tempo - o icone muda e os sons tocam
+
 import sys
 import os
 from playsound import playsound
@@ -16,28 +17,41 @@ from datetime import datetime, timedelta
 from PyQt5.QtCore import QRunnable, Qt, QThreadPool
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QSystemTrayIcon, QMenu, QAction, QLabel, QApplication, QMainWindow
-
 from configparser import ConfigParser
+
 cfg = ConfigParser()
-cfg.read('config.ini')
-print(cfg.sections())
+try:
+    cfg.read('config.ini')
+    time1_seconds = cfg.getint('pymodori','lotime') * 60
+    time2_seconds = cfg.getint('pymodori','hitime') * 60
+    tf = open('control','w')
+    tf.write('00:00:00')
+    tf.close()
+except:
+    #cfg('pymodori','lotime') = 20
+    #cfg('pymodori','hitime') = 25
+    #with open('config.ini', 'w') as configfile:
+    #    cfg.write(configfile)
+    time1_seconds = 20*60
+    time2_seconds = 25*60
 
-time1_seconds = cfg.getint('pymodori','lotime') * 60
-time2_seconds = cfg.getint('pymodori','hitime') * 60
-
-sound1_file   = 'data/sound1.mp3'
-sound2_file   = 'data/sound2.mp3'
-hap           = 'data/hap.png'
-sad           = 'data/sad.png'
-mad           = 'data/mad.png'
-
-
+## for now it's just useless because is not testing file, is adding string to variable
+try:
+    sound1_file   = 'data/sound1.mp3'
+    sound2_file   = 'data/sound2.mp3'
+    hap           = 'data/hap.png'
+    sad           = 'data/sad.png'
+    mad           = 'data/mad.png'
+except:
+    print("Where are my data files?")
+    sys.exit()
+                
 class Timer(QRunnable):
     def __init__(self, time1_seconds,time2_seconds):
         super().__init__()
         self.time1_seconds = time1_seconds
         self.time2_seconds = time2_seconds
-        print("HiTime set to: ", self.time1_seconds, " | LoTime set to: ", self.time2_seconds)
+        print("HiTime set to: ", self.time1_seconds/60, "min | LoTime set to: ", self.time2_seconds/60, "min")
     def runIT(self):
         init_time = datetime.now()
         ct1=0
@@ -48,14 +62,16 @@ class Timer(QRunnable):
                 f = open('control')
                 f.close()
             except IOError:
-                print("exit-timer")
+                print("Bye!")
                 x=False
+                AppExit()
+                
             if (init_time + timedelta(seconds=self.time1_seconds) < datetime.now()) and (ct1==0):
                 TrayDef.setIcon("sad")
                 playsound(sound1_file)
                 ct1=1
                 sleep(5)
-                print("LoTime")
+                #print("LoTime")
             elif init_time + timedelta(seconds=self.time2_seconds) < datetime.now():
                 TrayDef.setIcon("mad")
                 playsound(sound2_file)
@@ -63,10 +79,12 @@ class Timer(QRunnable):
                 ct1=0
                 sleep(5)
                 x=False
-                print("HiTime")
+                #print("HiTime")
             else:   
                 tdelta = init_time + timedelta(seconds=self.time2_seconds) - datetime.now()
-                print("Time2Buzz: ", tdelta)
+                tf = open('control','w')
+                tf.write(str(tdelta))
+                tf.close()
                 sleep(5)
 
 class Tray(QRunnable):
@@ -89,10 +107,10 @@ class Tray(QRunnable):
         # Menu Options
         self.o_quit.triggered.connect(AppExit) 
         self.o_quit.triggered.connect(sys.exit) 
-        self.o_about.triggered.connect(AboutWindow.show)
+        self.o_about.triggered.connect(AboutWindow.showme)
         #self.o_start.triggered.connect(TimerDef.runIT)
         self.o_start.triggered.connect(TWorker)        
-        self.o_settings.triggered.connect(SettingsWindow.show)
+        self.o_settings.triggered.connect(SettingsWindow.showme)
         self.tray.setContextMenu(self.menu) 
     
     #Change Tray Icon
@@ -114,18 +132,25 @@ class DefWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Settings")
+    def showme(self):
         label = QLabel("Pymodori!")
         label.setAlignment(Qt.AlignCenter)
         self.setCentralWidget(label)
+        self.show()
 
 class AboWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("About")
-        label = QLabel("Pomodori inspired App, but as a Python exercise.")
-        label.setAlignment(Qt.AlignCenter)
-        self.setCentralWidget(label)
-                
+        self.setWindowTitle("About Pymodori")    
+    def showme(self):
+        tf = open('control','r')
+        tlab = QLabel("Pomodori inspired App, \nbut as a Python exercise.\n - \n"+tf.read())
+        tf.close()
+        tlab.setWordWrap(True)
+        tlab.setAlignment(Qt.AlignCenter) 
+        self.setCentralWidget(tlab)
+        self.show()
+                      
 # MultiThread --
 # worker1 -> trayIcon
 # worker2 -> timer()
@@ -148,6 +173,7 @@ def main():
     mainControl = 0
     pool.CancelAll()
     os.remove('control') 
+
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
